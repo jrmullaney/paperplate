85064849fd7e05c0ab9b9ae816515ff4cb07ec4b
James Mullaney <j.mullaney@sheffield.ac.uk>
2017-09-12 22:56:56 +0100

    Querying MySQL and showing results, but orientation change not working

--- _class/simpleCMS.php
+++ _class/simpleCMS.php
@@ -1,43 +1,68 @@
 <?php
 
 class simpleCMS {
-    var $host;
-    var $username;
-    var $password;
-    var $table;
+  var $host;
+  var $username;
+  var $password;
+  var $table;
 
-    public function display_public() {
-        $q = "SELECT * FROM arXiv";
-        $r = mysql_query($q);
-    
-        if ( $r !== false && mysql_num_rows($r) > 0 ) {
-          while ( $a = mysql_fetch_assoc($r) ) {
-            $title = $a['title'];
-            $author = $a['prim_author'];
-    
-            $entry_display .= "<h2>$title</h2><p>$author</p>";
-    
-          }
-        } else {
-          $entry_display = <<<ENTRY_DISPLAY
-    
-<h2>This Page Is Under Construction</h2>;
-<p>
-No entries have been made on this page. 
-Please check back soon, or click the
-link below to add an entry!
-</p>
-    
+  public function display_public() {
+    $q = "SELECT * FROM arxiv";
+    $r = mysql_query($q);
+
+    if ( $r !== false && mysql_num_rows($r) > 0 ) {
+      while ( $a = mysql_fetch_assoc($r) ) {
+        $title = stripslashes($a['title']);
+        $author = stripslashes($a['prim_author']);
+
+        $entry_display .= <<<ENTRY_DISPLAY
+        <div class="grid-item">
+        <div id="title"><a href="http://www.diy.com">$title</a>
+        </div>
+        <div id="author">$author</div>
+        <div id="gridicon">
+        <a href="http://www.google.co.uk">
+        <img src="img/arxiv-logo.png" id="gridimage"></a>
+        </div>
+        <div id="gridicon">
+        <a href="http://www.sheffield.ac.uk">
+        <img src="img/pdf-logo.png" id="gridimage"></a>
+        </div>
+        <div id="gridicon">
+        <a href="http://www.dur.ac.uk">
+        <img src="img/ads-logo.png" id="gridimage"></a>
+        </div>
+        </div>
 ENTRY_DISPLAY;
-        }
-        return $entry_display;
       }
+    } else {
+      $entry_display = <<<ENTRY_DISPLAY
 
-    public function connect() {
-        mysql_connect($this->host,$this->username,$this->password) or die("Could not connect. " . mysql_error());
-        mysql_select_db($this->table) or die("Could not select database. " . mysql_error());
-    
-        #return $this->buildDB();
+    <h2>This Page Is Under Construction</h2>
+    <p>
+      No entries have been made on this page. 
+      Please check back soon, or click the
+      link below to add an entry!
+    </p>
+
+ENTRY_DISPLAY;
     }
+    $entry_display .= <<<ADMIN_OPTION
+
+    <p class="admin_link">
+      <a href="{$_SERVER['PHP_SELF']}?admin=1">Add a New Entry</a>
+    </p>
+
+ADMIN_OPTION;
+
+    return $entry_display;
+  }
+  
+  public function connect() {
+    mysql_connect($this->host,$this->username,$this->password) or die("Could not connect. " . mysql_error());
+    mysql_select_db($this->table) or die("Could not select database. " . mysql_error());
+    
+    #return $this->buildDB();
+  }
 }
 ?>
--- index.php
+++ index.php
@@ -1,108 +1,75 @@
 <!doctype html>
 <html class="no-js" lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>paperplate</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>paperplate</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
+      <link rel="apple-touch-icon" href="apple-touch-icon.png">
+      <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-        <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
-</head>
-<body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+      <link rel="stylesheet" href="css/normalize.css">
+      <link rel="stylesheet" href="css/main.css">
+      <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+      <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+  </head>
+  <body>
+        <!--[if lt IE 8]>ÏÏ
+   <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
 
 <!-- Add your site or application content here -->
-            <div class="topbar">
-	      <picture id="leftlogo">
-	        <source srcset="img/mlogo.png" media="(max-device-width: 760px)">
-	        <source srcset="img/logo.png">
-	        <img src="img/logo.png" id="leftlogo" style="width:auto;">
-	      </picture>
-	      <div class="icons">a</div>
-	      <div class="icons">a</div>
-	      <div class="icons">a</div>
-	    </div>
-	<div class="mainpanel">
-	  
-	<div class="grid" 
-	  data-isotope='{ "itemSelector": ".grid-item",
-	    "masonry": {"columnWidth":0, "fitWidth":true, "gutter": 5 } }'>
-	<div class="grid-item">
-	<div id="title"><a href="http://www.diy.com">
-	  Lorem ipsum dolor sit amet, consectetur adipiscing
-	  elit. Nunc viverra diam vel justo</a></div>
-	  <div id="author">J. Mullaney et al.</div>
-	  <div id="gridicon">
-	    <a href="http://www.google.co.uk">
-	    <img src="img/arxiv-logo.png" id="gridimage"></a></div>
-	  <div id="gridicon">
-	    <a href="http://www.sheffield.ac.uk">
-	    <img src="img/pdf-logo.png" id="gridimage"></a></div>
-	  <div id="gridicon">
-	    <a href="http://www.dur.ac.uk">
-	    <img src="img/ads-logo.png" id="gridimage"></a></div>
-	</div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-             <div class="grid-item"><div id="title">ahghk</div></div>
-          </div>
+    <div class="topbar">
+	    <picture id="leftlogo">
+	      <source srcset="img/mlogo.png" media="(max-device-width: 760px)">
+	      <source srcset="img/logo.png">
+	      <img src="img/logo.png" id="leftlogo" style="width:auto;">
+	    </picture>
+	    <div class="icons">a</div>
+	    <div class="icons">a</div>
+	    <div class="icons">a</div>
 	  </div>
+	
+    <div class="mainpanel">
+	    <div class="grid" 
+	      data-isotope='{ "itemSelector": ".grid-item",
+	      "masonry": {"columnWidth":0, "fitWidth":true, "gutter": 10 }}'>
+	      <?php
+
+          include_once('_class/simpleCMS.php');
+          $obj = new simpleCMS();
+          $obj->host = 'localhost';
+          $obj->username = 'James';
+          $obj->password = '8Y2wKwAD4';
+          $obj->table = 'paperplate';
+          $obj->connect();
+
+          if ( $_POST )
+            $obj->write($_POST);
+
+          echo ( $_GET['admin'] == 1 ) ? $obj->display_admin() : $obj->display_public();
+
+        ?>
+	    </div>  
+    </div>
 	  
-        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script>window.jQuery || document.write('<script                src="js/vendor/jquery-1.12.0.min.js"><\/script>')
+    </script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
-    </body>
+    <script>
+      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
+  </body>
 </html>
